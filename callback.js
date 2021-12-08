f4afef23a29899c42ee9f07344cdcfa9566876a7
OHMphatcha <phatcharaphol@hotmail.com>
2021-12-08 10:56:19 +0700

    Day1

---
+++ callback.js
@@ -0,0 +1,22 @@
+//ทบมวน callback function
+
+function calculate(x,y,callback){
+    console.log("กำลังคำนวณ...")
+    setTimeout(()=>{
+        const sum = x+y
+        callback(sum)
+    },3000)
+}
+
+// function display(result){
+//     console.log(`ผลบวก = ${result}`)
+// }
+
+// const sum = calculate(100,50)
+// display(sum)
+
+//ฟังก์ชั่นแบบ callback
+
+calculate(100,50,function(result){
+    console.log(`ผลบวก = ${result}`)
+})
---
+++ index.js
@@ -0,0 +1,5 @@
+console.log("เริ่มดาวน์โหลด")
+setTimeout(()=>{
+    console.log("กำลังดาวน์โหลด")
+},3000)
+console.log("จบการทำงาน")
